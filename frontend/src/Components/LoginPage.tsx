829eec7860a57197a87db617fc8592b958ead537
Carcion1 <thomasly@hotmail.co.uk>
2025-07-01 11:14:49 +0100

    adding loginpage, will need to cross reference api calls with axios

---
+++ frontend/src/Components/LoginPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const LoginPage = () => {
+    const testcall = async () => {await}
+  return <div>{/*research using forms. UseStates*/}</div>;
+};
+
+export default LoginPage;
