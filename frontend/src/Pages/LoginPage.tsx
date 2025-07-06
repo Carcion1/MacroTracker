fe49af491ffed59cef055032e0cc38c5d4014fdc
Carcion1 <thomasly@hotmail.co.uk>
2025-07-06 19:48:39 +0100

    Added login buttons, formatted email & password and back function

--- frontend/src/Pages/LoginPage.tsx
+++ frontend/src/Pages/LoginPage.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import "../Styles/LoginPage.css";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage: React.FC = () => {
+type Props = {};
+
+const LoginPage: React.FC<Props> = () => {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    //add login logic.
+  };
+  const handleBack = () => {
+    if (window.history.length > 1) {
+      navigate(-1); // if user navigates to login and then presses back, itll take them back to their original place.
+    } else {
+      navigate("/"); // if user lands directly on login page it'll take them back home
+    }
+  };
+
   return (
-    <div className="login-container">
-      <h2>Login</h2>
+    <div className="login-container-wrapper">
+      <div className="login-container">
+        <h2 className="login-header">Login</h2>
+        <div className="form-row">
+          <h4>Email:</h4>
+          <input type="email" className="input-field" />
+        </div>
+        <div className="form-row">
+          <h4>Password:</h4>
+          <input type="password" className="input-field" />
+        </div>
+        <div className="login-buttons">
+          <button className="back-button" onClick={handleBack}>
+            Back
+          </button>
+          <button className="login-button">Login</button>
+        </div>
+      </div>
     </div>
   );
 };
