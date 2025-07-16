2e06c15b01c3e0ed4c8b103404c6df9aa3c01a89
Carcion1 <thomasly@hotmail.co.uk>
2025-07-16 14:05:13 +0100

    adding login home nav bar

--- frontend/src/Pages/LoginHome.tsx
+++ frontend/src/Pages/LoginHome.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../Styles/LoginHome.css";
 
 const LoginHome = () => {
-  return <div></div>;
+  return (
+    <nav className="login-nav-bar">
+      <ul className="login-nav-links">
+        <li>
+          <Link to="/macrotracker" className="macro-tracker">
+            MacroTrack
+          </Link>
+        </li>
+        <li>
+          <Link to="/WeightTrack" className="weight-tracker">
+            WeightTrack
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default LoginHome;
